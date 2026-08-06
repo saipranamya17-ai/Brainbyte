a617d64aa043cde0ab58e174018680c09e1edb17
saipranamya17-ai <saipranamya17@gmail.com>
2026-08-06 13:34:52 +0530

    Add client/src/components/Layout.jsx

---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+
+export default function Layout() {
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Topbar />
+        <main className="flex-1 overflow-y-auto p-6">
+          <div className="max-w-6xl mx-auto page-enter">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
